7e81d43a0953d81a21d538f5c419d17c8050fc4d
Nik <130102204+niteazi@users.noreply.github.com>
2026-02-07 11:24:28 +0000

    add light dark mode

--- app/get_involved/page.tsx
+++ app/get_involved/page.tsx
@@ -1,8 +1,14 @@
+import { Button } from '@/components/ui/button';
+import { SaveIcon } from 'lucide-react';
+import { ThemeToggle } from "@/components/ThemeToggle";
+
 export default function GetInvolved() {
   return (
     <div>
+    <ThemeToggle />
       <h1>Get Involved</h1>
-      <p>Page content here</p>
+      
     </div>
   );
 }
+
